a292969c5dd1c62b395e022c271fcbf5aa35e107
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-11-23 15:19:54 +0530

    update the app

--- moneytracker/src/App.js
+++ moneytracker/src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>
+        400 <span>.000</span>
+      </h1>{" "}
+      <form>
+        <div className="basic">
+          <input type="text" placeholder={"20000 Mobile"} />
+          <input type="datetime-local"></input>
+        </div>
+        <div className="description">
+          <input type="text" placeholder={"description"}></input>
+        </div>
+        <button>Add new transaction</button>
+      </form>
+      <div className="transactions">
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Headphones</div>
+            <div className="description">
+              It was the time to buy new headphones , old were damaged
+            </div>
+          </div>
+          <div className="right">
+            <div className="price red">3000</div>
+            <div className="datetime">01-2024</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Headphones</div>
+            <div className="description">
+              It was the time to buy new headphones , old were damaged
+            </div>
+          </div>
+          <div className="right">
+            <div className="price green">3000</div>
+            <div className="datetime">01-2024</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Headphones</div>
+            <div className="description">
+              It was the time to buy new headphones , old were damaged
+            </div>
+          </div>
+          <div className="right">
+            <div className="price red">3000</div>
+            <div className="datetime">01-2024</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Headphones</div>
+            <div className="description">
+              It was the time to buy new headphones , old were damaged
+            </div>
+          </div>
+          <div className="right">
+            <div className="price green">3000</div>
+            <div className="datetime">01-2024</div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
